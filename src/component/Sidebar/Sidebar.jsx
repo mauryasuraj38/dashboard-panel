5078779319262e8d5ad67a77c6014e5f40ca2297
prabhakar <prabhakarkumar16181@gmail.com>
2023-12-15 13:59:11 -0800

    website responsive completed

--- src/component/Sidebar/Sidebar.jsx
+++ src/component/Sidebar/Sidebar.jsx
@@ -2,37 +2,58 @@ import Logo from "../../images/logo.png"
 import "./sidebar.css"
 import { SidebarData } from "../../Data/Data.js"
 import { useState } from "react";
+import { motion } from "framer-motion";
 
 function Sidebar() {
 
   const [selected, setSelected] = useState(0);
+  const [expanded, setExpanded] = useState(true);
+
+  const sidebarVariants = {
+    true: {
+      left: '0'
+    },
+    false:{
+      left: '-60%'
+    }
+  }
 
   return (
-    <div className="Sidebar">
-      {/* logo */}
-      <div className="logo">
-          <img src={Logo} alt="" />
-          <span>
-              Sh<span>o</span>ps
-          </span>
+    <>
+      <div className="bars"
+      style={expanded?{left:"60%"}:{left:"5%"}}
+      onClick={()=>setExpanded(!expanded)}
+      >
+      <i class="uil uil-bars"></i>
       </div>
-      {/* menu */}
-      <div className="menu">
-        {SidebarData.map((value,index) => (
-            <div className={selected===index?'menuItem active':'menuItem'}
-            key={index}
-            onClick={()=>setSelected(index)}
-            >
-              <i class={value.iconClass}></i>
-              <span>{value.heading}</span>
-            </div>
-        ))}
-        <div className="menuItem">
-            <i className="uil uil-sign-out-alt"></i>
+      <motion.div className="Sidebar"
+      variants={sidebarVariants}
+      animate={window.innerWidth<=768?`${expanded}`:''}
+      >
+        {/* logo */}
+        <div className="logo">
+            <img src={Logo} alt="" />
+            <span>
+                Sh<span>o</span>ps
+            </span>
         </div>
-      </div>
-
-    </div>
+        {/* menu */}
+        <div className="menu">
+          {SidebarData.map((value,index) => (
+              <div className={selected===index?'menuItem active':'menuItem'}
+              key={index}
+              onClick={()=>setSelected(index)}
+              >
+                <i class={value.iconClass}></i>
+                <span>{value.heading}</span>
+              </div>
+          ))}
+          <div className="menuItem">
+              <i className="uil uil-sign-out-alt"></i>
+          </div>
+        </div>
+      </motion.div>
+    </>
   )
 }
 
