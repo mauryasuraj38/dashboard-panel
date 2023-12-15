679765b96104a2606cc2e4476325eb73e5dfebdc
prabhakar <prabhakarkumar16181@gmail.com>
2023-12-14 19:14:16 -0800

    MainDash Recent Orders Completed

--- src/component/MainDash/MainDash.jsx
+++ src/component/MainDash/MainDash.jsx
@@ -1,11 +1,13 @@
 import "./MainDash.css"
 import Cards from "../Cards/Cards"
+import BasicTable from "../Table/Table"
 
 function MainDash() {
   return (
     <div className="MainDash">
       <h1>Dashboard</h1>
       <Cards />
+      <BasicTable />
     </div>
   )
 }
